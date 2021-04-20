12a3f0c71d96c1cd6846c942d96bec4f38d89b37
Lasagna-guy-develops <sebastianmontoyazuluaga@gmail.com>
2021-04-20 17:30:19 -0500

    update main, agregar html's

--- flaskr/main.py
+++ flaskr/main.py
@@ -4,20 +4,42 @@ from flask import request
 from flask import redirect
 from flask import url_for
 from flask import flash
+import DataBaseConnection
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/Login', methods=['GET', 'POST'])
 def login():
+    db = DataBaseConnection
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+
+        correo = request.form['username']
+        pssw = request.form['password']
+        q = 'select password from usuario where correo = %s;'
+        query = db.sql_query_var(q, (correo, ))
+
+        if(len(query)==0):
+            error = 'Invalid Credentials. Please try again.'
+        elif( pssw != query[0]['password']):
             error = 'Invalid Credentials. Please try again.'
         else:
             flash('You were successfully logged in')
             return redirect(url_for('home'))
     return render_template('Login.html', error=error)
 
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/Register', methods=['GET', 'POST'])
+def register():
+    db = DataBaseConnection
+    error = None
+    if request.method == 'POST':
+
+        correo = request.form['username']
+        pssw = request.form['password']
+
+
+    return render_template('SignUp.html')
+
+@app.route('/Home', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
